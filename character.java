cf9a7aec5ac8dd4d7dfa9369cf53a02b114c1b20
MLVei03 <vanlongveigar03@gmail.com>
2023-11-30 23:02:30 +0700

    Them character

---
+++ character.java
@@ -0,0 +1,28 @@
+/*class Animal {
+    public void makeSound() {
+        System.out.println("Some sound");
+    }
+}
+
+class Dog extends Animal {
+    public void makeSound() {
+        System.out.println("Bark");
+    }
+}
+
+class Cat extends Animal {
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+}
+
+public class character {
+    public static void main(String[] args) {
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myDog.makeSound(); // Output will be "Bark"
+        myCat.makeSound(); // Output will be "Meow"
+    }
+}
+*/
